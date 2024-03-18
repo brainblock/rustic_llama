9a624845d095648c9af1e244944a9b93fc4414b9
Zacarías F. Ojeda <zojeda@gmail.com>
2024-03-18 11:30:19 -0300

    adding args parsing

--- README.md
+++ README.md
@@ -7,6 +7,14 @@ A [Rust](https://www.rust-lang.org/) 🦀 based llama2 implementation
 * Writen based on [llama2.c](https://github.com/karpathy/llama2.c.git)
 * Created with the purpose of understanding the inference process in LLMs.
 
+## Run
+```bash 
+wget https://huggingface.co/karpathy/tinyllamas/resolve/main/stories15M.bin // default model
+wget https://huggingface.co/karpathy/tinyllamas/resolve/main/stories42M.bin
+wget https://huggingface.co/karpathy/tinyllamas/resolve/main/stories110M.bin
+
+$ cargo run --release -- --model_path=./stories15M.bin
+```
 
 ## Current Performance 
 50 tok/s 
@@ -15,3 +23,5 @@ A [Rust](https://www.rust-lang.org/) 🦀 based llama2 implementation
 CPU: Intel(R) Core(TM) i7-1065G7 CPU @ 1.30GHz
 MemTotal:       32620848 kB
 ```
+
+![flamegraph](./flamegraph.svg)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use clap::Parser;
+
 mod generator;
 mod sampler;
 mod tokenizer;
@@ -9,16 +11,50 @@ use tokenizer::Tokenizer;
 
 use humansize::{format_size, DECIMAL};
 
+/// Rustic Lama
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Model path.
+    #[arg(short = 'p', long = "model_path", default_value_t = String::from("./stories15M.bin"))]
+    model_path: String,
+
+    /// Tokenizer path.
+    #[arg(short = 't', long = "tokenizer_path", default_value_t = String::from("./tokenizer.bin"))]
+    tokenizer_path: String,
+
+    /// Temperature in search.
+    #[arg(short = 'v', long = "temperature", default_value_t = 0.9)]
+    temperature: f32,
+
+    /// Number of steps in search.
+    #[arg(short = 'n', long = "n_steps", default_value_t = 300)]
+    n_steps: usize,
+
+    /// Whether to use lazy model loading via mmap.
+    #[arg(short = 'm', long = "is_mmap", default_value_t = false)]
+    is_mmap: bool,
+
+    /// Initial prompt
+    #[arg(short = 's', long = "prompt", default_value_t = String::from(""))]
+    prompt: String,
+}
+
 fn main() {
-    let mut sampler = Sampler::new(0.0f32);
-    let tokenizer: Tokenizer = Tokenizer::new("tokenizer.bin".into(), 32000).unwrap();
-    let transformer = Transformer::new("stories15M.bin".into()).unwrap();
+    let args = Args::parse();
+    println!("using options: {:?}\n", args);
+
+
+    let mut sampler = Sampler::new(args.temperature);
+    let tokenizer: Tokenizer = Tokenizer::new(args.tokenizer_path.into(), 32000).unwrap();
+    let transformer = Transformer::new(args.model_path.into()).unwrap();
 
     let transformer_size = datasize::data_size(&transformer);
+
     eprintln!("config: {:?}", transformer.config());
     eprintln!(
         "transformer_size heap: {}",
         format_size(transformer_size, DECIMAL)
     );
-    generator::generate(&transformer, &tokenizer, &mut sampler, None, 300).unwrap();
+    generator::generate(&transformer, &tokenizer, &mut sampler, None, args.n_steps).unwrap();
 }
